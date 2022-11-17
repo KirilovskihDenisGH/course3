2d563d80b758fb601dd341e1d822656a7017444a
aristokrat991 <geeky.post@bk.ru>
2022-11-17 15:37:10 +0500

    1

---
+++ course/pages/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+def mainPageView(request):
+    return render(request, "main.html")
+
+def filmsPageView(request):
+    return render(request, "films.html")
+
+def filmsPageView(request):
+    return render(request, "cartoons.html")
+
+def filmsPageView(request):
+    return render(request, "serials.html")
+
+def filmsPageView(request):
+    return render(request, "anime.html")
