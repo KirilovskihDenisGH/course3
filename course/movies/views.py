5a9eb6f30e32443050fdc50d78a47c2bdf204859
aristokrat991 <geeky.post@bk.ru>
2022-12-15 06:29:12 +0500

    comments 1.1.1

--- course/movies/views.py
+++ course/movies/views.py
@@ -2,32 +2,32 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views.generic.base import View
 from django.views.generic import ListView, DetailView, UpdateView, DeleteView
-from . import models
+from .models import Movie, Reviews
 from .forms import ReviwForm
 
 class mainPageView(ListView):
-    model = models.Movie
+    model = Movie
     template_name = "main.html"
-    # queryset = models.Movie(id=1)
+
 
 class moviePageView(ListView):
-    model = models.Movie
+    model = Movie
     template_name = "movies.html"
 
 class cartoonsPageView(ListView):
-    model = models.Movie
+    model = Movie
     template_name = "cartoons.html"
 
 class serialsPageView(ListView):
-    model = models.Movie
+    model = Movie
     template_name = "serials.html"
 
 class animePageView(ListView):
-    model = models.Movie
+    model = Movie
     template_name = "anime.html"
 
 class movieDetailView(DetailView):
-    model = models.Movie
+    model = Movie
     template_name = "detail.html"
 
 class AddReview(View):
@@ -40,11 +40,11 @@ class AddReview(View):
         return redirect("/")
 
 class ReviewUpdateView(UpdateView):
-    model = models.Reviews
+    model = Reviews
     template_name = 'review_edit.html'
     fields = ['text']
     success_url = reverse_lazy('main')
 class ReviewDeleteView(DeleteView):
-    model = models.Reviews
+    model = Reviews
     template_name = 'review_delete.html'
     success_url = reverse_lazy('main')
